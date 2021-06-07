ef14774b7222f8c249a9f899d4a5ea7d7aaf834a
廉竣为 <534897141@qq.com>
2021-06-07 20:26:52 +0800

    前后端按约定创建ResultVO类。包含业务码/错误信息/数据

---
+++ src/main/java/com/example/demo/common/ResultVo.java
@@ -0,0 +1,22 @@
+package com.example.demo.common;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data  // 注在类上，提供类的get、set、equals、hashCode、canEqual、toString方法
+@Builder
+public class ResultVo {
+    private int code;
+    private String message;
+    private Map<String,Object> data;
+
+   public static ResultVo success(Map<String,Object> data){
+       return ResultVo.builder().code(200).data(data).build();
+   }
+
+   public static ResultVo error(int code,String message){
+       return ResultVo.builder().code(code).message(message).build();
+   }
+}
